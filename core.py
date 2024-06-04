38760bc41706433295cb56658857a111abb96061
Tand-Anthros <thunder.light.pegasys@gmail.com>
2024-06-04 13:14:22 +0300

    rollback and minor complite working on console interface

--- core.py
+++ core.py
@@ -3,11 +3,9 @@ import platform, json, os, sys, time
 
 def file(path, data = None):
     if data == None:
-        try:
-            with open(path, 'r') as file:
-                return json.load(file)
-        except FileNotFoundError:
-            return {}
+        try: 
+            with open(path, 'r') as file: return json.load(file)
+        except FileNotFoundError: return {}
 
     with open(path, 'w') as file:
         json.dump(data, file, indent=4)
@@ -23,43 +21,33 @@ def convert(value):
     if value == '': return ''
     if '\n' not in value and value[0] in ['{', '[', '('] or value in ['True', 'False', 'None']:   
         try: return eval(value)
-        except SyntaxError: return None     
+        except SyntaxError: input(f'SyntaxError...')
     else:
         try: return int(value)
         except ValueError: 
             try: return float(value)
-            except ValueError: return None
+            except ValueError: return value
 
-
-def delete(root, key):
-    answer = interface(f'delete "{key}"?', '', f'(+, y, yes) > ')
-    if answer.lower() in ['+', 'y', 'yes', 'д', 'да']: 
-        root.pop(key)
-    return root
-
-def update(root, key, value):
-    root[key] = value
-    return root
-
-
-root = file(sys.argv[1])
-# ctxs = {}
-# objs = {}
-acts = {'$delete': delete, '$update': update}
 
 while True:
     try:
-        key = interface(
-            '/root:', ', '.join(root.keys()), '',
-            # '/#ctx\'s:', ', '.join(ctxs.keys()), '',
-            # '/@obj\'s:', ', '.join(objs.keys()), '',
-            '/$act\'s:', ', '.join(acts.keys()), '',
-            '"key?" > ')
+        root = file(sys.argv[1])
+        key = interface('/root:', ', '.join(root.keys()), '', '"key?" > ')
+        if key == '': continue
         
-        value = interface(f'/root:', ', '.join(root.keys()), '', f'"{key}": {root.get(key)}', f'"{key}":? > ')
+        if type(root.get(key)).__name__ == 'str': content = f'"{root.get(key)}"'
+        else: content = root.get(key)
+        value = interface(f'/root:', ', '.join(root.keys()), '', f'"{key}": {content}', '', f'"{key}":? > ')
         value = convert(value)
 
-        if key in acts.keys() and type(value).__name__ == 'tuple': root = acts[key](root, *value)
+        if value == '': continue
+        elif value == None:
+            answer = interface(f'delete "{key}"?', '', f'(+, y, yes) > ')
+            if answer.lower() in ['+', 'y', 'yes', 'д', 'да']: 
+                try: root.pop(key)
+                except KeyError: pass
+            else: root[key] = value
+        else: root[key] = value
         
         file(sys.argv[1], root)
 
